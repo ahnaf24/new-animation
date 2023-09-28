f5fb4a79f4fe1be9a310f57baa3267c923b848af
Test User <ahnaf.shakil24@gmail.com>
2023-09-28 13:14:51 +0600

    b

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext('2d');
 canvas.width = 500;
 canvas.height = 700;
 const explosions = [];
-
+let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion{
     constructor(x, y){
@@ -11,5 +11,22 @@ class Explosion{
         this.y = y; 
         this.spriteWidth = 200;
         this.spriteHeight = 179;
+        this.width = this.spriteWidth/2;
+        this.height = this.spriteHeight/2;
+        this.image = new Image();
+        this.image.src = 'boom.png';
+        this.frame = 0;
+    }
+    update(){
+        this.frame++;
+    }
+    draw(){
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
+
+window.addEventListener('click', function(e){
+    let positionX = canvasPosition.left;
+    let positionY = canvasPosition.top;
+    explosions.push(new Explosion)
+});
