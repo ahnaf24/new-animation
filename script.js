d932d4c201625c3f71d67e8c48259df582c65168
Test User <ahnaf.shakil24@gmail.com>
2023-10-01 08:43:16 +0600

    adding sound

--- script.js
+++ script.js
@@ -11,14 +11,18 @@ class Explosion{
         this.spriteHeight = 179;
         this.width = this.spriteWidth * 0.7;
         this.height = this.spriteHeight * 0.7;
-        this.x = x - this.width/2;
-        this.y = y - this.height/2; 
+        this.x = x;
+        this.y = y;  
         this.image = new Image();
         this.image.src = 'boom.png';
         this.frame = 0;
         this.timer = 0;
+        this.angle = Math.random() * 6.2;
+        this.sound = new Audio();
+        this.sound.src = 'booom.wav';
     }
     update(){
+        if(this.frame === 0) this.sound.play();
         this.timer++;
         if(this.timer % 10 === 0){
           this.frame++;
@@ -28,17 +32,18 @@ class Explosion{
     draw(){
         ctx.save();
         ctx.translate(this.x, this.y);
-        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.rotate(this.angle);
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, 0 - this.width/2, 0 - this.height/2, this.width, this.height);
         ctx.restore();
     }
 }
 
-window.addEventListener('mousemove', function(e){
-   createAnimation();
-});
-function createAnimation(){
-    let positionX = canvasPosition.left;
-    let positionY = canvasPosition.top;
+// window.addEventListener('mousemove', function(e){
+//    createAnimation(e);
+// });
+function createAnimation(e){
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.y - canvasPosition.top;
     explosions.push(new Explosion(positionX, positionY));
 }
 
